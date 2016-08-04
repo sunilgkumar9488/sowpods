10420a7b29b82b17d1289dd02d8d92dc409e3c46
Nisarg Thakkar <nisargtha@gmail.com>
2016-08-04 11:20:13 +0530

    added interfaces

--- Anagrams.java
+++ Anagrams.java
@@ -1,4 +1,24 @@
 public class Anagrams{
+
+	public String sortString(String str){
+		return null;
+	}
+
+	public String HashMap<String, ArrayList<String> > getAnagramMap(ArrayList<String> inputLines){
+		return null;
+	}
+
+	public static void printAnagramMap(HashMap<String, ArrayList<String>> anagramMap){
+		return;
+	}
+
+	public static ArrayList<String> readInput(){
+		return null;
+	}
+
 	public static void main(String[] args){
+
+
+
 	}
 }
